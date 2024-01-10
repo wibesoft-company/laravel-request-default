7c8dbab8e85c9a932ef4a56c8f0d9e1161add209
Oktay Yılmaz <oktayyilmaz090.work@gmail.com>
2024-01-10 22:09:47 +0300

    Update InputDefaulter.php
    Fix Request Safe

--- src/Traits/InputDefaulter.php
+++ src/Traits/InputDefaulter.php
@@ -12,9 +12,12 @@ trait InputDefaulter
 
     protected function defaulter()
     {
+
         $defaults = $this->defaults();
         foreach ($defaults as $key => $value) {
+
             if (!$this->has($key)) {
+                $this->validator->setData([$key => $value] +  $this->validator->getData());
                 $this->merge([$key => $value]);
             }
         }
